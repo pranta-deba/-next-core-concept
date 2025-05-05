cac3d6c81b41925d97f85a949f246f54bf728458
Pranto Deb <rajprit0m@outlook.com>
2025-05-05 23:31:53 +0600

    [Connnetc DB]

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 const uri = process.env.DATABASE_URL!;
 
-export const dbConnect = async (collectionName: string) => {
+const dbConnect = async (collectionName: string) => {
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
@@ -12,3 +12,6 @@ export const dbConnect = async (collectionName: string) => {
 
   return client.db(process.env.DB_NAME).collection(collectionName);
 };
+
+
+export default dbConnect;
