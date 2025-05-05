e73fdc43067ba9b8a6f3f17399556b47ae3e31d8
Pranto Deb <rajprit0m@outlook.com>
2025-05-05 23:18:33 +0600

    [Create API]

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,7 +1,18 @@
+import { NextRequest } from "next/server";
+
 export async function GET() {
   const data = {
     success: true,
     message: "Data fetched successfully",
+    status: 200,
+  };
+
+  return Response.json({ data });
+}
+export async function POST(req: NextRequest) {
+  const data = {
+    success: true,
+    message: "Data created successfully",
     status: 200,
   };
 
