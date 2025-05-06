cccb84d7f7ab7aac9636e3cf77822eedbabbf5d7
Pranto Deb <rajprit0m@outlook.com>
2025-05-06 23:15:11 +0600

    [#]

--- src/coms/Form.tsx
+++
@@ -1,9 +0,0 @@
-const Form = () => {
-    return (
-        <div>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est provident rerum distinctio molestiae excepturi vitae commodi impedit assumenda quod! Veniam amet laudantium cumque delectus ullam consectetur fugiat vel dolor magni?</h1>
-        </div>
-    );
-};
-
-export default Form;
