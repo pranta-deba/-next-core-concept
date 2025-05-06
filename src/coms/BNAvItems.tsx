ca7a3ee2b2c65c1469b2810974f0889afffd1cc0
Pranto Deb <rajprit0m@outlook.com>
2025-05-06 23:15:02 +0600

    [#]

--- src/coms/BNAvItems.tsx
+++
@@ -1,9 +0,0 @@
-const BNAvItems = () => {
-    return (
-        <div>
-            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam temporibus atque voluptate suscipit fugit ipsum optio similique, debitis repellat provident nulla quam eos quaerat rerum nam beatae harum quasi id!</h1>
-        </div>
-    );
-};
-
-export default BNAvItems;
