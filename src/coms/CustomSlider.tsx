fa060a36b851e8999b40ef8258a5899391d28e1b
Pranto Deb <rajprit0m@outlook.com>
2025-05-06 23:14:18 +0600

    [#]

---
+++ src/coms/CustomSlider.tsx
@@ -0,0 +1,9 @@
+const CustomSlider = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit doloremque numquam esse eius fuga dignissimos quis voluptatum provident consequatur quae modi quasi ipsa, aperiam excepturi, culpa perferendis itaque quos. Amet.</h1>
+        </div>
+    );
+};
+
+export default CustomSlider;
